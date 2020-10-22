4e50d41d10962157570654d23da39d5691328f5f
Maria Alarcon <mariagaa993@gmail.com>
2020-10-22 06:43:34 -0400

    Updates

---
+++ precache-manifest.2b26554a64746deb45f8b198ea82fb1b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c29137d9b47a453ea642d7471c9d0965",
+    "url": "/photo-gallery-app/index.html"
+  },
+  {
+    "revision": "a9f76071fa8dfca68555",
+    "url": "/photo-gallery-app/static/css/main.d56ac0cf.chunk.css"
+  },
+  {
+    "revision": "bf68d9cbf2cc4a198309",
+    "url": "/photo-gallery-app/static/js/2.f7d5d4de.chunk.js"
+  },
+  {
+    "revision": "d2ff67a5f290a85b508a595a768d7cfc",
+    "url": "/photo-gallery-app/static/js/2.f7d5d4de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f76071fa8dfca68555",
+    "url": "/photo-gallery-app/static/js/main.852fd92b.chunk.js"
+  },
+  {
+    "revision": "edf92699a8d9f71f0331",
+    "url": "/photo-gallery-app/static/js/runtime-main.14c04d24.js"
+  },
+  {
+    "revision": "919a9f74f2584bc307470e382be27f27",
+    "url": "/photo-gallery-app/static/media/camera.919a9f74.svg"
+  },
+  {
+    "revision": "aeec28632f4519bdb1c51013fead8ccd",
+    "url": "/photo-gallery-app/static/media/close-button.aeec2863.svg"
+  }
+]);
